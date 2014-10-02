633bc7b5f1aa517bb7607aabea99b714b3c3277b
Tomi Mäenpää <tomi@h1.fi>
2014-10-02 10:09:28 +0300

    updated from _s

--- archive.php
+++ archive.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying Archive pages.
+ * The template for displaying archive pages.
  *
  * Learn more: http://codex.wordpress.org/Template_Hierarchy
  *
@@ -30,16 +30,19 @@ get_header(); ?>
 							printf( __( 'Day: %s', 'h1-theme' ), '<span>' . get_the_date() . '</span>' );
 
 						elseif ( is_month() ) :
-							printf( __( 'Month: %s', 'h1-theme' ), '<span>' . get_the_date( 'F Y' ) . '</span>' );
+							printf( __( 'Month: %s', 'h1-theme' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'h1-theme' ) ) . '</span>' );
 
 						elseif ( is_year() ) :
-							printf( __( 'Year: %s', 'h1-theme' ), '<span>' . get_the_date( 'Y' ) . '</span>' );
+							printf( __( 'Year: %s', 'h1-theme' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'h1-theme' ) ) . '</span>' );
 
 						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
 							_e( 'Asides', 'h1-theme' );
 
+						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+							_e( 'Galleries', 'h1-theme' );
+
 						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
-							_e( 'Images', 'h1-theme');
+							_e( 'Images', 'h1-theme' );
 
 						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
 							_e( 'Videos', 'h1-theme' );
@@ -49,6 +52,15 @@ get_header(); ?>
 
 						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
 							_e( 'Links', 'h1-theme' );
+
+						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+							_e( 'Statuses', 'h1-theme' );
+
+						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+							_e( 'Audios', 'h1-theme' );
+
+						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+							_e( 'Chats', 'h1-theme' );
 
 						else :
 							_e( 'Archives', 'h1-theme' );
@@ -73,6 +85,10 @@ get_header(); ?>
 					 * If you want to override this in a child theme, then include a file
 					 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
 					 */
+
+					/* Consider using get_post_format() if you are supporting Post Formats in your theme.
+					 */
+
 					get_template_part( 'parts/entry', get_post_type() );
 				?>
 
