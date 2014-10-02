1e839afe0f861ff0ab419449bce141419fef17fe
Tomi Mäenpää <tomi@h1.fi>
2014-10-02 10:15:32 +0300

    updated from _s

--- footer.php
+++ footer.php
@@ -12,10 +12,9 @@
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info">
-			<?php do_action( 'h1_credits' ); ?>
-			<a href="http://wordpress.org/" rel="generator"><?php printf( __( 'Proudly powered by %s', 'h1-theme' ), 'WordPress' ); ?></a>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'h1-theme' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'h1-theme' ), 'WordPress' ); ?></a>
 			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %1$s by %2$s.', 'h1-theme' ), 'H1 Theme', '<a href="http://h1.fi" rel="designer">H1</a>' ); ?>
+			<?php printf( __( 'Theme: %1$s by %2$s.', 'h1-theme' ), 'h1-theme', '<a href="http://h1.fi" rel="designer">H1</a>' ); ?>
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 </div><!-- #page -->
