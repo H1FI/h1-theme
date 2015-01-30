191e9a9a932e2b79983984b7023fda4cdebde990
Tomi Mäenpää <tomi@h1.fi>
2015-01-30 14:48:40 +0200

    Added walkers
    - Renamed navigation-walkers -> walkers
    - Added new custom menu walker which preserves custom css classes defined in wp-admin, fixes #6
    - Added new category walker

--- functions.php
+++ functions.php
@@ -34,6 +34,14 @@ function h1_setup() {
 	add_theme_support( 'automatic-feed-links' );
 
 	/*
+	 * Let WordPress manage the document title.
+	 * By adding theme support, we declare that this theme does not use a
+	 * hard-coded <title> tag in the document head, and expect WordPress to
+	 * provide it for us.
+	 */
+	add_theme_support( 'title-tag' );
+
+	/*
 	 * Enable support for Post Thumbnails on posts and pages.
 	 *
 	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
@@ -44,7 +52,7 @@ function h1_setup() {
 	register_nav_menus( array(
 		'primary' => __( 'Primary Menu', 'h1-theme' ),
 	) );
-	
+
 	/*
 	 * Switch default core markup for search form, comment form, and comments
 	 * to output valid HTML5.
@@ -53,7 +61,10 @@ function h1_setup() {
 		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
 	) );
 
-	// Enable support for Post Formats.
+	/*
+	 * Enable support for Post Formats.
+	 * See http://codex.wordpress.org/Post_Formats
+	 */
 	//add_theme_support( 'post-formats', array( 'aside', 'image', 'video', 'quote', 'link' ) );
 
 	// Setup the WordPress core custom background feature.
@@ -69,35 +80,45 @@ add_action( 'after_setup_theme', 'h1_setup' );
  * Enqueue scripts and styles.
  */
 function h1_scripts() {
+
+	/**
+	 * Directories for scripts and styles.
+	 */
 	$css_dir = get_stylesheet_directory_uri() . '/assets/styles/css';
 	$js_dir  = get_stylesheet_directory_uri() . '/assets/js/';
 
 	/**
-	 * CSS
+	 * Register scripts.
 	 */
-
-	wp_enqueue_style( 'h1-stylesheet', $css_dir . '/app.css' );
-
-	// Legacy CSS (no mediaqueries, px instead of rem)
-	wp_enqueue_style( 'h1-legacy-stylesheet', $css_dir . '/app-no-mq.css' );
-
-	// Add Legacy CSS based on conditional tags
-	global $wp_styles;
-	$wp_styles->registered['h1-legacy-stylesheet']->add_data( 'conditional', 'lt IE 9' );
+	wp_register_script( 'h1-js', $js_dir . '/min/built.min.js', array( 'jquery' ), null, true );
+	wp_register_script( 'h1-js-dev', $js_dir . '/dev/built.js', array( 'jquery' ), null, true );
 
 	/**
-	 * JavaScript
+	 * Register styles.
 	 */
+	wp_register_style( 'h1-stylesheet', $css_dir . '/app.css' );
+	wp_register_style( 'h1-stylesheet-legacy', $css_dir . '/app-no-mq.css' ); // No mediaqueries, px instead of rem
 
+	/**
+	 * Enqueue styles.
+	 */
+	wp_enqueue_style( 'h1-stylesheet' );
+
+	global $wp_styles;
+	$wp_styles->registered['h1-stylesheet-legacy']->add_data( 'conditional', 'lt IE 9' ); // Add legacy CSS for IE8 and below.
+
+
+	/**
+	 * Enqueue scripts.
+	 */
 	wp_enqueue_script( 'jquery' );
 
 	if ( defined( 'WP_ENV' ) && WP_ENV == 'development' ) {
-		wp_enqueue_script( 'h1-js', $js_dir . '/dev/built.js', array( 'jquery' ), null, true );
+		wp_enqueue_script( 'h1-js-dev' );
 	} else {
-		wp_enqueue_script( 'h1-js', $js_dir . '/min/built.min.js', array( 'jquery' ), null, true );
+		wp_enqueue_script( 'h1-js' );
 	}
 
-	// Comment out or remove if you don't need commenting
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
@@ -105,7 +126,12 @@ function h1_scripts() {
 add_action( 'wp_enqueue_scripts', 'h1_scripts' );
 
 /**
- * Define sidebars and possible custom widgets.
+ * Implement the Custom Header feature.
+ */
+//require get_template_directory() . '/functions/custom-header.php';
+
+/**
+ * Define widget areas and possible custom widgets.
  */
 require get_stylesheet_directory() . '/functions/widgets.php';
 
@@ -120,9 +146,9 @@ require get_template_directory() . '/functions/foundation.php';
 require get_template_directory() . '/functions/navigation.php';
 
 /**
- * Load Custom walkers for use in nav menus
+ * Load Custom Walkers.
  */
-// require get_template_directory() . '/functions/navigation-walkers.php';
+require get_template_directory() . '/functions/walkers.php';
 
 /**
  * Custom template tags for this theme.
@@ -133,6 +159,11 @@ require get_template_directory() . '/functions/template-tags.php';
  * Custom functions that act independently of the theme templates.
  */
 require get_template_directory() . '/functions/extras.php';
+
+/**
+ * Customizer additions.
+ */
+require get_template_directory() . '/functions/customizer.php';
 
 /**
  * Load Jetpack compatibility file.
--- functions/jetpack.php
+++ functions/jetpack.php
@@ -10,10 +10,10 @@
  * Add theme support for Infinite Scroll.
  * See: http://jetpack.me/support/infinite-scroll/
  */
-function _h1_jetpack_setup() {
+function h1_jetpack_setup() {
 	add_theme_support( 'infinite-scroll', array(
 		'container' => 'main',
 		'footer'    => 'page',
 	) );
 }
-add_action( 'after_setup_theme', '_h1_jetpack_setup' );
+add_action( 'after_setup_theme', 'h1_jetpack_setup' );
