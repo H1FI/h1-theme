71a8945c8adeeccef100896eaea94efdb15a05e6
Tomi Mäenpää <tomi@h1.fi>
2015-02-04 10:36:40 +0200

    Updated Grunt default JS includes
    wp/* was including customizer.js which has no use outside of WordPress customizer, the JS files are now defined per script.

--- assets/Gruntfile.js
+++ assets/Gruntfile.js
@@ -13,7 +13,8 @@ module.exports = function(grunt) {
 			  src: [
 			  	// 'bower_components/foundation/js/foundation/foundation.js',
 			  	// 'bower_components/foundation/js/foundation/foundation.dropdown.js'
-			  	'js/dev/wp/*',
+			  	'js/dev/wp/navigation.js',
+			  	'js/dev/wp/skip-link-focus.js',
 			  	'js/dev/app.js'
 			  	],
 			  dest: 'js/dev/built.js',
